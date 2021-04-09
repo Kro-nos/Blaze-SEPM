c4689887ff450e21dcfed787ad9994f702968e88
Sine-theta <81235360+Sine-theta@users.noreply.github.com>
2021-04-09 23:17:23 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+//LOGOUT.PHP
+
+session_start();
+unset($_SESSION["username"]);
+unset($_SESSION["password"]);
+header("Location:user_login.php");
+//// destroy all $_SESSION variables
+session_destroy();
+setcookie (session_name(), '', time()-3600);
+
+
+echo "<script>alert('You have successfully logged out.')</script>";
+echo "<script>window.open('index.php','_self')</script>";
+
+?>
