f16504e98996adb835a4c2e92f05cb02abe50fe0
Sine-theta <81235360+Sine-theta@users.noreply.github.com>
2021-04-10 15:41:20 +0530

    Update About.php

--- About.php
+++ About.php
@@ -44,13 +44,7 @@ if (array_key_exists('Admin', $_SESSION) === false) {
             <?php } elseif ($_SESSION['Admin'] == '-1') {?><a href="/index.php">HOME</a>
             <?php }?></strong></li>
 						<li><strong><a href="statusretriever.php">LIVE STATUS</a></strong></li>
-<!--						<li><strong><a href="Ticket/ticket.php">TICKET RESERVATION</a></strong></li>  -->
-						<li><strong><div class="dropdown"><a class="dropbtn">ENQUIRY</a>
-							<div class="dropdown-content">
-							<a href="traind/traind.php">Train Details</a>
-							<a href="fare/fare.php">Fare Enquiry</a>
-							<a href="Cancelled_trains/Cancelled_trains.php">Cancelled_trains</a>
-							<a href="Train_route/Route_retriever.php">Train Route Information</a></div>
+
 						</div></strong></li>
 						<li><strong><a href="About.php" class="selected">ABOUT</a></strong></li>
 						<li><strong><a href="Team.php">TEAM</a></strong></li>
