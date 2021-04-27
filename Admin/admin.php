765b19c95579e846dd5503ce19b7d45f127818dc
Sine-theta <81235360+Sine-theta@users.noreply.github.com>
2021-04-27 21:51:42 +0530

    Update admin.php

--- Admin/admin.php
+++ Admin/admin.php
@@ -30,9 +30,7 @@ $u = explode("@", $row['username']);
 				<nav class="nav">
 				<div class="container">
 	    <div class="navbar-header">
-	    	<a href="#" class="pull-left ">
-	    <div id="logo" alt="TheAppleTalks Logo"></div>
-	    </a>
+	    	
 	    <div class="navbar-brand">
 	    </div></div></div></nav>
 				<nav id="nav" class="">
